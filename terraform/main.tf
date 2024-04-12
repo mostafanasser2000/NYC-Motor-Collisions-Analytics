379f6d3b703e848ed998aeef0d1c8774c2fcbdc1
mostafanasser2000 <mustafanasser503@gmail.com>
2024-04-13 00:04:13 +0200

    add terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.6.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentionals)
+  project = var.project
+  region = var.region
+}
+
+resource "google_storage_bucket" "collisions-bucket" {
+  name = var.gcp_bucket_name
+  location = var.location
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
