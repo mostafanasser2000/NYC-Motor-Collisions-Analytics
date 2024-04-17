7498b6a321df574a591a1ead27540424fe19c60e
mostafa nasser <mustafanasser503@gmail.com>
2024-04-17 22:23:15 +0000

    add staging models

---
+++ dbt/models/staging/stg_crash_data.sql
@@ -0,0 +1,42 @@
+with crashes as (
+    select * from {{ source('staging', 'crashes') }}
+    where collision_id is not null and DATE(crash_datetime) between '2013-01-01' AND '2023-12-31'
+)
+
+select
+    -- identifiers
+    cast(collision_id as integer) as collision_id,
+    -- timestamp
+    cast(crash_datetime as timestamp) as crash_datetime,
+
+    -- crash info
+    borough,
+    coalesce(cast(number_of_persons_injured as integer), 0) as number_of_persons_injured,
+    coalesce(cast(number_of_persons_killed as integer), 0) as number_of_persons_killed,
+    coalesce(cast(number_of_pedestrians_injured as integer), 0) as number_of_pedestrians_injured,
+    coalesce(cast(number_of_pedestrians_killed as integer),0) as number_of_pedestrians_killed,
+    coalesce(cast(number_of_cyclist_injured as integer), 0) as number_of_cyclist_injured,
+    coalesce(cast(number_of_cyclist_killed as integer),0 ) as number_of_cyclist_killed,
+    coalesce(cast(number_of_motorist_injured as integer), 0) as number_of_motorist_injured,
+    coalesce(cast(number_of_motorist_killed as integer), 0) as number_of_motorist_killed,
+    contributing_factor_vehicle_1,
+    contributing_factor_vehicle_2,
+    contributing_factor_vehicle_3,
+    contributing_factor_vehicle_4,
+    contributing_factor_vehicle_5,
+    vehicle_type_code_1,
+    vehicle_type_code_2,
+    vehicle_type_code_3,
+    vehicle_type_code_4,
+    vehicle_type_code_5
+
+from crashes
+
+
+
+
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
---
+++ dbt/models/staging/stg_person_data.sql
@@ -0,0 +1,32 @@
+with persons as (
+    select * from {{ source('staging', 'persons') }}
+    where collision_id is not null and DATE(crash_datetime) between '2013-01-01' AND '2023-12-31'
+)
+
+select
+    -- identifiers
+    cast(unique_id as integer) as unique_id,
+    cast(collision_id as integer) as collision_id,
+    -- timestamp
+    cast(crash_datetime as timestamp) as crash_datetime,
+
+    -- person info
+    person_type,
+    person_age,
+    {{get_person_sex_type("person_sex")}} as person_sex,
+    person_injury,
+    safety_equipment,
+    emotional_status,
+    bodily_injury
+
+
+from persons
+
+
+
+
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
